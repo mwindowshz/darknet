1c4d69fae8b674c86d09d735b6412243ac9e14fd
mwindowshz <mwindowsHz@gmail.com>
2018-01-28 15:24:43 +0200

    mutex IDetector using global instance

--- src/yolo_v2_class.hpp
+++ src/yolo_v2_class.hpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <deque>
 #include <algorithm>
-
+#include <mutex>
 #ifdef OPENCV
 #include <opencv2/opencv.hpp>			// C++
 #include "opencv2/highgui/highgui_c.h"	// C
@@ -24,31 +24,31 @@
 #endif
 #endif
 
-struct bbox_t {
-	unsigned int x, y, w, h;	// (x,y) - top-left corner, (w, h) - width & height of bounded box
-	float prob;					// confidence - probability that the object was found correctly
-	unsigned int obj_id;		// class of object - from range [0, classes-1]
-	unsigned int track_id;		// tracking id for video (0 - untracked, 1 - inf - tracked object)
-};
+//struct bbox_t {
+//	unsigned int x, y, w, h;	// (x,y) - top-left corner, (w, h) - width & height of bounded box
+//	float prob;					// confidence - probability that the object was found correctly
+//	unsigned int obj_id;		// class of object - from range [0, classes-1]
+//	unsigned int track_id;		// tracking id for video (0 - untracked, 1 - inf - tracked object)
+//};
+//
+//struct image_t {
+//	int h;						// height
+//	int w;						// width
+//	int c;						// number of chanels (3 - for RGB)
+//	float *data;				// pointer to the image data
+//};
 
-struct image_t {
-	int h;						// height
-	int w;						// width
-	int c;						// number of chanels (3 - for RGB)
-	float *data;				// pointer to the image data
-};
-
-
-class Detector {
+class Detector : public IDetector{
 	std::shared_ptr<void> detector_gpu_ptr;
 public:
 	float nms = .4;
-
+	YOLODLL_API Detector(const char* cfg_filename, const char* weight_filename, int gpu_id = 0);
 	YOLODLL_API Detector(std::string cfg_filename, std::string weight_filename, int gpu_id = 0);
 	YOLODLL_API ~Detector();
 
 	YOLODLL_API std::vector<bbox_t> detect(std::string image_filename, float thresh = 0.2, bool use_mean = false);
 	YOLODLL_API std::vector<bbox_t> detect(image_t img, float thresh = 0.2, bool use_mean = false);
+	YOLODLL_API int detectResArray(image_t img, float thresh = 0.2, bool use_mean = false,bbox_t** pBoxes = NULL);
 	static YOLODLL_API image_t load_image(std::string image_filename);
 	static YOLODLL_API void free_image(image_t m);
 	YOLODLL_API int get_net_width();
